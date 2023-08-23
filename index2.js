d9c3f0c57bfa9db5e8dbbd9a73aa961bc4777742
naekkoch <micaelaas3105@gmail.com>
2023-08-22 21:52:50 -0300

    exercicios

---
+++ index2.js
@@ -0,0 +1,63 @@
+
+/*
+
+exercicio 1
+resposta a) false
+resposta b) false
+resposta c) true
+resposta d) boolean
+*/
+
+/*
+
+exercicio 2
+resposta; sera impresso no console somente a palavra "soma"
+
+exercicio 3
+resposta; deveria ser:
+
+let primeiroNumero = prompt("Digite um numero!")
+let segundoNumero = prompt("Digite outro numero!")
+
+const resultado = primeiroNumero + segundoNumero
+
+console.log(resultado)
+
+*/
+/*
+const idade = prompt ("Qual sua idade")
+const idadeAmigo = prompt ("Qual a idade seu(sua) melhor amigo(a)?")
+console.log("Sua idade é maior do que a do(a) seu melhor amigo(a)?", idade >= idadeAmigo)
+*/
+/*
+const numero = prompt ("Me diga um número par")
+const restoDaDivisao = numero % 2
+console.log(restoDaDivisao)
+
+
+const idade = prompt ("Qual sua idade em anos")
+
+const a = 12
+const b = 365
+const c = 24
+const resultado = idade * a 
+const resposta = idade * b
+const horas = idade * c
+console.log("Você teve ",resultado," meses ",resposta," dias do ano e ",horas," horas durante sua vida")
+
+exercicio 4
+
+
+const numeros = prompt ("Me diga um numero de 1 a 100")
+const maisUm = prompt ("Outro numero de 1 a 100")
+const a = numeros> maisUm
+const b = numeros === maisUm
+const c = numeros % maisUm === 0
+const d = maisUm % numeros === 0
+console.log("O primeiro numero é maior que segundo?",a)
+console.log("O primeiro numero é igual ao segundo?", b)
+console.log("O primeiro numero é divisível pelo segundo?", c)
+console.log("O segundo numero é divisível pelo primeiro?",d)
+*/
+
+
